5359227a47c53e71e23623ec8b159a73f5919c49
sjz <sjz1987@126.com>
2018-11-01 08:52:28 +0800

    websocket

--- src/main/java/com/sz/example/websocket/WebsocketConfig.java
+++ src/main/java/com/sz/example/websocket/WebsocketConfig.java
@@ -3,14 +3,14 @@ package com.sz.example.websocket;
 import javax.annotation.Resource;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
+
+//@EnableWebMvc
 @Configuration
-@EnableWebMvc
 @EnableWebSocket
 public class WebsocketConfig extends WebMvcConfigurerAdapter implements WebSocketConfigurer {
     
